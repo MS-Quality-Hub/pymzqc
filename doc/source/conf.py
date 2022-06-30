cfc21a68b0a2020cd37fd31bc481dd93295a760e
mwalzer <walzer@ebi.ac.uk>
2022-06-30 18:09:10 +0000

    style improvements to readthedocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -46,7 +46,10 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
-html_style = 'css/pzmzqc_theme.css' 
+html_theme_options = {
+    'github_repo': 'MS-Quality-hub/pymzqc',
+    'page_width': '80%',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
