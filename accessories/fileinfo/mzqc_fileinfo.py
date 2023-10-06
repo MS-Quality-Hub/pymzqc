630fed09ed1f8eb62a9ba2e24b23c2464aa920e9
mwalzer <walzer@ebi.ac.uk>
2023-10-06 16:21:59 +0000

    added creationDate + optional run completion time to mzqc-fileinfo

--- accessories/fileinfo/mzqc_fileinfo.py
+++ accessories/fileinfo/mzqc_fileinfo.py
@@ -11,7 +11,10 @@ from mzqc.MZQCFile import JsonSerialisable as mzqc_io
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 INFO = '''
-The selected mzQC file has {n} different metrics registered, from {m} different runs and {k} defined 'sets'. 
+The selected mzQC file has {n} different metrics registered, 
+from {m} different runs and {k} defined 'sets', 
+and it was created @ {d}.
+
 '''
 def print_help():
     """
@@ -39,7 +42,7 @@ def mzqcfileinfo(input):
     rs = len(infofile.runQualities)
     ss = len(infofile.setQualities)
     mets = len({ metr.name for qm in chain(infofile.runQualities, infofile.setQualities) for metr in qm.qualityMetrics })
-    print(INFO.format(n=mets, m=rs, k=ss) )
+    print(INFO.format(n=mets, m=rs, k=ss, d=infofile.creationDate) )
 
     for n, run in enumerate(infofile.runQualities, start=1):
         mets = { (metr.name, metr.accession) for metr in run.qualityMetrics }
@@ -50,15 +53,17 @@ def mzqcfileinfo(input):
             print(u"\t💾 {n} \n\t\t@ {l} \n\t\tof type".format(n=file.name, l=file.location), 
                   getattr(file.fileFormat, "name", "UNSPECIFIED"))
 
-        ct_str = next(iter([cd.value for infi in run.metadata.inputFiles for cd in infi.fileProperties if cd.accession=="MS:1000747"]))
-        if ct_str.endswith("Z"):
-            ct_str = ct_str[:-1]
-        try: 
-            ct_datetime_object = dt.datetime.fromisoformat(ct_str)
-            print(u"\t🕑 The MS run object was completed at", ct_datetime_object.isoformat())
+        try:
+            ct_str = next(iter([cd.value for infi in run.metadata.inputFiles for cd in infi.fileProperties if cd.accession=="MS:1000747"]))
+            if ct_str.endswith("Z"):
+                ct_str = ct_str[:-1]
+            try: 
+                ct_datetime_object = dt.datetime.fromisoformat(ct_str)
+                print(u"\t🕑 The MS run object was completed at", ct_datetime_object.isoformat())
+            except Exception as e:
+                print("\tCould not extract the run's completion time! Wrong format? Use the validator to find out.") 
         except Exception as e:
-            print("\tCould not extract the run's completion time! Wrong format? Use the validator to find out.") 
-    
+            print("\tNo completion time found in mzQC for this MS run object.") 
         print("\tMetrics:")
         print(*[u"\t 📈" + str(m) for m in mets], sep='\n',)
 
